9bf13b524b14995c645ad8ffc37a2d8f2dc8d3bf
Michael Futral <Futralmc@dukes.jmu.edu>
2022-02-16 16:49:33 -0500

    -m

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -4,7 +4,7 @@ package com.h2;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
@@ -12,6 +12,21 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+
+        sum = 0;
+        for(i=0, i <= numbers[i].length, i++) {
+            number[i]+sum;
+        }
+        return sum;
+
+        return int number*2;
     }
+
+    private static add(int[] numbers) {
+        return int -1;
+    }
+
+
 }
+
+
